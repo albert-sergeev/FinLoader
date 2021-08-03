3daf73713c9707f6f2f101f5bb5941c1fe999aa6
Albert Sergeyev <albert_s_s@mail.ru>
2021-08-03 17:10:18 +1000

    connect btnImport FinamFile and thread to process data (and do data transmit btw processes)

--- importfinamform.cpp
+++ importfinamform.cpp
@@ -779,6 +779,7 @@ void ImportFinamForm::slotShowByNamesChecked(int Checked)
 void ImportFinamForm::slotBtnImportClicked()
 {
 
+
     if(bReadyToImport){
         QItemSelectionModel  *qml = ui->viewTickers->selectionModel();
         auto lst (qml->selectedIndexes());
